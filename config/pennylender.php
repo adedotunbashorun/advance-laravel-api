7fd88474842fcbd6e3031b3e4519c7d112d5feb7
adedotunbashorun <adedotunolawale@gmail.com>
2019-11-25 00:57:55 +0100

    minor fixes

--- config/pennylender.php
+++ config/pennylender.php
@@ -2,15 +2,8 @@
  return [
     "payment" => [
         "paystack" => [
-            "secret_key" => "sk_test_bbd247058597034d4164696798b3910d0cb98261",
-            "public_key" => "pk_test_f53388f6e32543777e80d123e6de584db3eefb53"
+            "secret_key" => "",
+            "public_key" => ""
         ]
     ],
-    "gemini" => [
-        "base_url" => "https://pennylender.geminiapp.net/api/apirequest.asmx?wsdl&wt=json",
-        "client_id" => "UEdPTERGUkVFRE9NQ0FSRA==",
-        "api_key" => "x3FyEzUtyPyuf/KNgB1bK73LnVOsS1sR9eXu6YB8iOE="
-    ],
-    "interest" => 4,
-    "admin_fee" => 2.9
  ];
